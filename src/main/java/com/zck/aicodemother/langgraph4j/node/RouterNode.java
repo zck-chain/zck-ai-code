87212b8ac0d32b03de2d3e8a23c838b4e7096016
zck <11191311+kanggee@user.noreply.gitee.com>
2026-04-15 18:40:16 +0800

    后端:ai工作流（langGraph4j）

---
+++ src/main/java/com/zck/aicodemother/langgraph4j/node/RouterNode.java
@@ -0,0 +1,29 @@
+package com.zck.aicodemother.langgraph4j.node;
+
+import com.zck.aicodemother.langgraph4j.state.WorkflowContext;
+import com.zck.aicodemother.model.enums.CodeGenTypeEnum;
+import lombok.extern.slf4j.Slf4j;
+import org.bsc.langgraph4j.action.AsyncNodeAction;
+import org.bsc.langgraph4j.prebuilt.MessagesState;
+
+import static org.bsc.langgraph4j.action.AsyncNodeAction.node_async;
+
+@Slf4j
+public class RouterNode {
+    public static AsyncNodeAction<MessagesState<String>> create() {
+        return node_async(state -> {
+            WorkflowContext context = WorkflowContext.getContext(state);
+            log.info("执行节点: 智能路由");
+            
+            // TODO: 实际执行智能路由逻辑
+            
+            // 简单的假数据
+            CodeGenTypeEnum generationType = CodeGenTypeEnum.HTML;
+            // 更新状态
+            context.setCurrentStep("智能路由");
+            context.setGenerationType(generationType);
+            log.info("路由决策完成，选择类型: {}", generationType.getText());
+            return WorkflowContext.saveContext(context);
+        });
+    }
+}
