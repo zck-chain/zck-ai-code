555449c7845159ad2475055d04981e639ffbd9e4
康哥 <11191311+kanggee@user.noreply.gitee.com>
2026-05-07 19:51:35 +0800

    后端:代码部署修改一些代码

--- src/main/java/com/zck/aicodemother/config/RedisChatMemoryStoreConfig.java
+++ src/main/java/com/zck/aicodemother/config/RedisChatMemoryStoreConfig.java
@@ -1,6 +1,7 @@
 package com.zck.aicodemother.config;
 
-import cn.hutool.core.util.StrUtil;
+
+import com.github.xiaoymin.knife4j.core.util.StrUtil;
 import dev.langchain4j.community.store.memory.chat.redis.RedisChatMemoryStore;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
